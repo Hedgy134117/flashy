f9da446949c4c2052b4d98203b555b4297d374a8
Hedgy134117 <hedgy134117@gmail.com>
2020-08-11 18:33:55 -0400

    Fix the Secret Key

--- backend/flashy/flashy/settings.py
+++ backend/flashy/flashy/settings.py
@@ -11,10 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-<<<<<<< Updated upstream
-=======
 import secrets
->>>>>>> Stashed changes
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,11 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-<<<<<<< Updated upstream
-SECRET_KEY = '$-70rg6#_rx1syzrm75f_4f8ucj)&aqyc064f(+d6gv1eku_u6'
-=======
-SECRET_KEY = secrets.SECRE_KEY
->>>>>>> Stashed changes
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
