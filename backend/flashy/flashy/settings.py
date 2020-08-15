04449a9d11000c79e14bcda0d4350dffdce8962b
Hedgy134117 <hedgy134117@gmail.com>
2020-08-15 16:25:53 -0400

    Allow XMLHttp requests using CORS middleware

--- backend/flashy/flashy/settings.py
+++ backend/flashy/flashy/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'cards.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'flashy.urls'
